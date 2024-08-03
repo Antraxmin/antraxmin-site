c54390040bb724f87597af30ea6afe41fb9851b6
Antraxmin <antraxmin@naver.com>
2024-08-03 23:46:40 +0900

    프로젝트 정보 업데이트

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,7 +3,7 @@ import { Project } from '@/types';
 
 const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
   return (
-    <div className="rounded-lg shadow-md overflow-hidden transition-all duration-300 hover:shadow-lg hover:scale-105 w-full max-w-xs mx-auto bg-gray-800">
+    <div className="rounded-lg shadow-md overflow-hidden transition-all duration-300 hover:shadow-lg hover:scale-105 w-full max-w-xs mx-auto bg-[#252626]">
       <div className="relative w-full" style={{ paddingTop: '56.25%' }}>
         <Image 
           src={project.image} 
@@ -12,10 +12,10 @@ const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
           objectFit="cover"
         />
       </div>
-      <div className="p-4 bg-gray-800"> 
-        <h4 className="font-bold text-sm bg-gray-800 mb-1 text-white truncate">{project.title}</h4>
-        <p className="text-gray-300 text-xs bg-gray-800 text-xs mb-2 line-clamp-2">{project.description}</p>
-        <div className="bg-gray-800 flex flex-wrap gap-1">
+      <div className="p-4 bg-[#252626]"> 
+        <h4 className="font-bold text-sm bg-[#252626] mb-1 text-white truncate">{project.title}</h4>
+        <p className="text-gray-300 text-xs bg-[#252626] text-xs mb-2 line-clamp-2">{project.description}</p>
+        <div className="bg-[#252626] flex flex-wrap gap-1">
           {project.technologies.map((tech, index) => (
             <span 
               key={index} 
--- src/data/project.ts
+++ src/data/project.ts
@@ -6,8 +6,52 @@ export const projects: Project[] = [
     description: "간지나는 나만의 velog 뱃지",
     image: "/projects/velog-github-badge.png",
     technologies: [
-        { name: "nest.js", color: "bg-gray-400" },
-        { name: "graphql", color: "bg-red-500" },
+        { name: "Nest.js", color: "bg-gray-400" },
+        { name: "GraphQL", color: "bg-red-500" },
+      ],
+  },
+  {
+    title: "SOON+",
+    description: "순천향대 학생들을 위한 교내 종합 정보 제공 플랫폼",
+    image: "/projects/soon-plus.png",
+    technologies: [
+        { name: "Next.js", color: "bg-gray-400" },
+        { name: "React", color: "bg-gray-400" },
+      ],
+  },
+  {
+    title: "CodeGen AI",
+    description: "생성형 AI 기반 알고리즘 문제 자동 제작 서비스",
+    image: "/projects/codegen-ai.png",
+    technologies: [
+        { name: "Next.js", color: "bg-gray-400" },
+        { name: "Chat GPT", color: "bg-gray-400" },
+      ],
+  },
+  {
+    title: "Thisper",
+    description: "AI 기반 유튜브 댓글 분석 및 대화 플랫폼",
+    image: "/projects/thisper.png",
+    technologies: [
+        { name: "Python", color: "bg-gray-400" },
+        { name: "FastAPI", color: "bg-gray-400" },
+        { name: "GCP", color: "bg-gray-400" },
+      ],
+  },
+  {
+    title: "Focus-Guard",
+    description: "주의력 분산 방지를 위한 크롬 확장 프로그램",
+    image: "/projects/focus-guard.png",
+    technologies: [
+        { name: "Chrome Extension", color: "bg-gray-400" },
+      ],
+  },
+  {
+    title: "Seoul-Bike-Analysis",
+    description: "서울시 공공자전거 이용현황 알림 웹서비스",
+    image: "/projects/seoul-bike.png",
+    technologies: [
+        { name: "Python", color: "bg-gray-400" },
       ],
   },
 ];
