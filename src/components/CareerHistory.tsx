6c47df159ae727915f282cdf4f77dbe0dcb8c0da
Antraxmin <antraxmin@naver.com>
2024-08-03 17:38:35 +0900

    types 및 data 분리

--- src/components/CareerHistory.tsx
+++ src/components/CareerHistory.tsx
@@ -1,57 +1,6 @@
+import { experiences } from '@/data';
+import { Experience } from '@/types';
 import Image from 'next/image';
-
-type Experience = {
-  organization: string;
-  logo: string;
-  role: string;
-  period: string;
-  type: 'education' | 'career' | 'startup';
-};
-
-const experiences: Experience[] = [
-    {
-      organization: '순천향대학교',
-      logo: '/logos/sch.png',
-      role: '컴퓨터소프트웨어공학과',
-      period: '21.03 → 25.02(예정)',
-      type: 'education',
-    },
-    {
-      organization: 'ECOHI',
-      logo: '/logos/ecohi.jpeg',
-      role: 'Software Engineer',
-      period: '24.04 → ING',
-      type: 'career',
-    },
-    {
-      organization: 'COMING SOON',
-      logo: '/logos/comingsoon.jpeg',
-      role: '교내 IT 서비스 개발동아리',
-      period: '24.03 → ING',
-      type: 'education',
-    },
-    {
-      organization: 'CREPT',
-      logo: '/logos/crept.png',
-      role: '순천향대학교 창업동아리',
-      period: '23.10 → 24.02',
-      type: 'startup',
-    },
-    {
-      organization: '멋쟁이사자처럼 11기',
-      logo: '/logos/likelion.png',
-      role: '대학생 IT 창업 동아리 멋쟁이사자처럼',
-      period: '23.03 → 23.09',
-      type: 'education',
-    },
-    {
-      organization: 'GDSH SCH 4th',
-      logo: '/logos/gdsc.png',
-      role: 'App Track Member',
-      period: '22.07 → 23.07',
-      type: 'education',
-    },
-  ];
 
 const getBadgeColor = (type: Experience['type']) => {
   switch (type) {
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export type Experience = {
+    organization: string;
+    logo: string;
+    role: string;
+    period: string;
+    type: 'education' | 'career' | 'startup';
+  };
