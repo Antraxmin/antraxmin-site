77aee7d96b8222a7c12c542bf50283ab6a7345be
Antraxmin <antraxmin@naver.com>
2024-08-04 17:01:56 +0900

    title 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Antraxmin',
+  title: '성장하는 개발자, 임채민',
   description: '',
 }
 
