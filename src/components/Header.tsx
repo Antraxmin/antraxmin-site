17ce7e30310259154bb0141dd42aa3af3c62c800
Antraxmin <antraxmin@naver.com>
2024-08-05 23:11:26 +0900

    Profile Hero 컴포넌트 분리

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import ProfileHero from './ProfileHero';
 
 const Header = () => {
   return (
@@ -17,20 +18,7 @@ const Header = () => {
           </nav>
         </div>
       </div>
-      <section>
-        <div className="flex items-center mb-4">
-          <Image src="/profile.png" alt="임채민" width={80} height={80} className="rounded-full mr-4" />
-          <div>
-            <h2 className="text-xl font-bold">임채민</h2>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <p>E-mail: antraxmin@gmail.com</p>
-        <div className="flex space-x-4 mt-2">
-          <Link href="#" className="hover:text-gray-300">Github</Link>
-          <Link href="#" className="hover:text-gray-300">LinkedIn</Link>
-        </div>
-      </section>
+      <ProfileHero />
     </header>
   );
 };
---
+++ src/components/ProfileHero.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const ProfileHero = () => {
+    return <section>
+    <div className="flex items-center mb-4">
+      <Image src="/profile.png" alt="임채민" width={80} height={80} className="rounded-full mr-4" />
+      <div>
+        <h2 className="text-xl font-bold">임채민</h2>
+        <p>Software Engineer</p>
+      </div>
+    </div>
+    <p>E-mail: antraxmin@gmail.com</p>
+    <div className="flex space-x-4 mt-2">
+      <Link href="#" className="hover:text-gray-300">Github</Link>
+      <Link href="#" className="hover:text-gray-300">LinkedIn</Link>
+    </div>
+  </section>
+}
+
+export default ProfileHero;
