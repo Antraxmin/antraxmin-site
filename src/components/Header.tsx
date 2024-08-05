928e40b0b12922ed2f2f413033b177e13955f5d6
Antraxmin <antraxmin@naver.com>
2024-08-05 22:40:54 +0900

    Header 카테고리 옵션 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,20 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="mb-12">
-      <h1 className="text-2xl font-bold mb-8">Antraxmin</h1>
+    <header className="mb-12 relative">
+      <div className="flex justify-between items-center mb-8">
+        <h1 className="text-2xl font-bold">Antraxmin</h1>
+        <div className="flex items-center space-x-4">
+          <nav className="flex space-x-4">
+            <Link href="/" className="hover:text-gray-300">
+              Profile
+            </Link>
+            <Link href="/blog" className="hover:text-gray-300">
+              Blog
+            </Link>
+          </nav>
+        </div>
+      </div>
       <section>
         <div className="flex items-center mb-4">
           <Image src="/profile.png" alt="임채민" width={80} height={80} className="rounded-full mr-4" />
@@ -15,9 +27,8 @@ const Header = () => {
         </div>
         <p>E-mail: antraxmin@gmail.com</p>
         <div className="flex space-x-4 mt-2">
-          <Link href="#">Blog</Link>
-          <Link href="#">Github</Link>
-          <Link href="#">LinkedIn</Link>
+          <Link href="#" className="hover:text-gray-300">Github</Link>
+          <Link href="#" className="hover:text-gray-300">LinkedIn</Link>
         </div>
       </section>
     </header>
