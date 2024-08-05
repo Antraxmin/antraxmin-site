e54645c336c993859703be93dcb734d8d7d3ad16
Antraxmin <antraxmin@naver.com>
2024-08-06 00:07:56 +0900

    tailwindcss/typography 적용

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,22 +1,18 @@
-import { getAllPosts, getPostBySlug } from '@/lib/getPosts';
-import markdownToHtml from '@/lib/markdownToHtml';
+import { getPostBySlug } from '@/lib/getPosts'
+import markdownToHtml from '@/lib/markdownToHtml'
 
-export async function generateStaticParams() {
-  const posts = getAllPosts();
-  return posts.map((post) => ({
-    slug: post.slug,
-  }));
-}
-
-export default async function Post({ params }: { params: { slug: string } }) {
-  const post = getPostBySlug(params.slug);
-  const content = await markdownToHtml(post.content || '');
+export default async function BlogPost({ params }: { params: { slug: string } }) {
+  const post = getPostBySlug(params.slug)
+  const content = await markdownToHtml(post.content || '')
 
   return (
-    <article className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
-      <div className="text-gray-600 mb-4">{post.date}</div>
-      <div className="prose max-w-none" dangerouslySetInnerHTML={{ __html: content }} />
+    <article className="mt-8">
+      <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+      <div className="text-gray-400 mb-8">{post.date}</div>
+      <div 
+        className="prose prose-invert prose-lg max-w-none"
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
     </article>
-  );
+  )
 }
