d4db55335c039fab09539a2d46f85ed07a099ba7
Antraxmin <antraxmin@naver.com>
2024-08-05 23:08:58 +0900

    전체 글 가져오기 로직 구현

---
+++ src/lib/getPosts.ts
@@ -0,0 +1,41 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+const postsDirectory = path.join(process.cwd(), 'posts')
+
+export function getAllPosts() {
+  const fileNames = fs.readdirSync(postsDirectory)
+  const allPostsData = fileNames.map(fileName => {
+    const slug = fileName.replace(/\.md$/, '')
+
+    const fullPath = path.join(postsDirectory, fileName)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+    const matterResult = matter(fileContents)
+
+    return {
+      slug,
+      ...(matterResult.data as { date: string; title: string; excerpt: string })
+    }
+  })
+
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1
+    } else {
+      return -1
+    }
+  })
+}
+
+export function getPostBySlug(slug: string) {
+  const fullPath = path.join(postsDirectory, `${slug}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  const { data, content } = matter(fileContents)
+
+  return {
+    slug,
+    content,
+    ...(data as { date: string; title: string; excerpt: string })
+  }
+}
